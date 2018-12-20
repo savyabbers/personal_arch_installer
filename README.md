4c04a52b6927c85115362c86890650eb98e765c3
savyabbers <adamvince24@gmail.com>
2018-12-20 01:14:59 -0500

    Changed the readme.md and the script should be able to install on both bios boot or efi boot

--- README.md
+++ README.md
@@ -1,26 +1,17 @@
 # Personal Install script.
 
--------------------------------------------------
-## Untested Script
-    Use at your own risk.
--------------------------------------------------
+
 ## Usage of script
     ./install -d /dev/xxx
--------------------------------------------------
+
 Personal install script to make arch a little easier to install.
 
-For now this script will assume that you have 3 partitions on a disk that you are installing arch on.
-1st partition is the 550M fat32 bootloader for systemd
-2nd partition is your root drive.
-3rd is the swap.
-
-You'll have to make the partitions on a GPT table drive
+In the installer The script will have you fill out the correct partitioning tables on to the selected harddirve you chose. then will try to install and use the correct boot loader based on if the efi folder exests or not
 
 The script will install some of the common tools used for developers and install the KDE desktop with the base applications.
 
--------------------------------------------------
+
 
 ## Things todo...
 * Add automatic disk partitioning
 * extra command flag to install listed packages
-* be able to install BIOS and EFI boot modes.
--- install.sh
+++ install.sh
@@ -8,6 +8,13 @@ if [ ! -e "/sys/firmware/efi/efivars" ] ; then
     exit 1
 fi
 
+#implementation of to do uefi or bios installs
+if [ ! -e "/sys/firmware/efi/efivars" ] ; then
+    efi="0"
+else
+    efi="1"
+fi
+
 #want to check if we are connected to internet
 #install needs to be connected to internet
 wget -q --spider https://archlinux.org/
@@ -16,72 +23,54 @@ if [ $? -ne 0 ] ; then
     exit 1
 fi
 
-#maybe want to make use select a harddisk to install on
-if [  ] ; then
-    echo "No HardDrive selected"
-    echo "Please use 'install /dev/xvy'"
-    exit 1
-fi
 
 while getopts "d:" opts ; do
     case ops in
-        d)Disk="$OPTARG"    ;;
+        d)DISK="$OPTARG"    ;;
     esac
 done
 
 if [ $# -eq 0 ] ; then
-    echo "Usage: $(basename $0) [-d HardDrive]"
-    echo "  This script ecpects to have a harddisk with 3 partions on it."
-    echo "      dev/xxx1 - being the boot"
-    echo "      dev/xxx2 - being the root"
-    echo "      dev/xxx3 - being the swap"
-    echo "  \nYou do not need to format. just make the disk sectors for now"
+    cat << EOI 
+Usage: $(basename $0) [-d HardDrive]
+    The harddrive that you input as an arg is used for the installation
+    Later in the installation it will call up cfdisk to have the user
+    input the partition. This script follows a Boot, Root, Swap partition.
+EOI
     exit 0
 fi
 
 #partition the disk
-#partition will follow as
-# Boot sector 514M
-# Root / Max - Ram
-# Swap for Ram
-# we will also want to name the root os as arch_os
-#
-#fdisk $1 << EOF
-#n
-#p
-#1
-#
-#w
-#EOF
+cfdisk -z $DISK
 
 # partition check
-if [ ! -e "${Disk}1" ] ; then
+if [ ! -e "${DISK}1" ] ; then
     echo "No boot partition"
     exit 1
 fi
 
-if [ ! -e "${Disk}2" ] ; then
+if [ ! -e "${DISK}2" ] ; then
     echo "No root partition"
     exit 1
 fi
 
-if [ ! -e "${Disk}3" ] ; then
+if [ ! -e "${DISK}3" ] ; then
     echo "No swap Partition"
     exit 1
 fi
 
 #label root partition
-e2label "${Disk}2" arch_os
+e2label "${DISK}2" arch_os
 
 #format the partition
-mkfs.fat -F32 "${Disk}1" 
-mkfs.ext4 "${Disk}2"
-mkswap "${Disk}3"
-swapon "${Disk}3"
+mkfs.fat -F32 "${DISK}1" 
+mkfs.ext4 "${DISK}2"
+mkswap "${DISK}3"
+swapon "${DISK}3"
 
-mount "${Disk}2" /mnt
+mount "${DISK}2" /mnt
 mkdir /mnt/boot
-mount "${Disk}1" /mnt/boot
+mount "${DISK}1" /mnt/boot
 
 # get pacman-contrib to rank list
 wget -O mirrorlist "https://www.archlinux.org/mirrorlist/?country=US&protocol=https"
@@ -101,7 +90,9 @@ pacstrap /mnt $base $kde $extras $moredev
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # arch-chroot
-arch-chroot /mnt << EOF
+
+if [ $efi ] ; then  #if in efi
+    arch-chroot /mnt << EOF
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 hwclock --systohc
 printf "en_US.UTF8 UTF8\n" >> /etc/locale.gen
@@ -115,12 +106,28 @@ bootctl --path=/boot/ install
 exit
 EOF
 
+    #fixing files in new comuter for proper boot
+    printf "default\tarch\ntimeout\t5\neditor\tno\n" > /mnt/boot/loader/loader.conf
+    #entry
+    printf "title\tArch Linux\nlinux \tvmlinuz-linux\n" >> /mnt/boot/loader/entries/arch.conf
+    printf "initrd\t/initramfs-linux.img\n" >> /mnt/boot/loader/entries/arch.conf
+    printf "options\troot=LABEL=arch_os rw\n" >> /mnt/boot/loader/entries/arch.conf
+    
+else    #if not in efi
+    arch-chroot /mnt << EOF
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc
+printf "en_US.UTF8 UTF8\n" >> /etc/locale.gen
+local-gen
+printf "LANG=en_US.UTF8\n" >> /etc/locale.conf
+printf "hostname" > /etc/hostname
+rustup toolchain install stable
+rustup defult stable
+pacman -S grub
+grub-install -target=i368-pc $DISK
+passwd
+EOF
 
-#fixing files in new comuter for proper boot
-printf "default\tarch\ntimeout\t5\neditor\tno\n" > /mnt/boot/loader/loader.conf
-#entry
-printf "title\tArch Linux\nlinux \tvmlinuz-linux\n" >> /mnt/boot/loader/entries/arch.conf
-printf "initrd\t/initramfs-linux.img\n" >> /mnt/boot/loader/entries/arch.conf
-printf "options\troot=LABEL=arch_os rw\n" >> /mnt/boot/loader/entries/arch.conf
+fi
 
-
+echo "install Done!"
