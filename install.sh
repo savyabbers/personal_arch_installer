f55ad902c5108d3579000a863ca60902639ee261
savyabbers <adamvince24@gmail.com>
2020-02-20 21:44:45 -0500

    added openssh, wget, zip, unzip, man, texinfo to base package

--- install.sh
+++ install.sh
@@ -79,7 +79,7 @@ rm /etc/pacman.d/mirrorlist
 mv mirrorlist /etc/pacman.d/mirrorlist
 
 # packages to be installed
-base="base base-devel xorg-server ufw linux linux-firmware dhcpcd networkmanager"
+base="base base-devel xorg-server ufw linux linux-firmware dhcpcd networkmanager openssh wget zip unzip man texinfo"
 kde="plasma kdebase yakuake spectacle kdeconnect okular kate sddm kcalc"
 extras="firefox ark libreoffice-fresh sshfs deluge filezilla vlc nano vim emacs"
 moredev="ghc rustup lua erlang clisp nasm git jdk8-openjdk"
